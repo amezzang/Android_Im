efedffaeb077e6a34bf59a9379a60d4e98ec6387
김예솔 <ssul4666@naver.com>
2020-07-02 17:17:40 +0900

    intent 전화걸기

--- Intent/app/src/main/java/com/example/intent/MainActivity.java
+++ Intent/app/src/main/java/com/example/intent/MainActivity.java
@@ -15,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn1 = findViewById(R.id.button);
+        Button btn1 = findViewById(R.id.button); // 인터넷 접속 버튼
+        Button btn2 = findViewById(R.id.button2); // 전화 걸기 버튼
 
         /* onclicklistener + alt+ enter > 필요한메소드 자동으로 추가 */
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-9559-4666"));
+                startActivity(intent);
+            }
+        });
     }
 }
